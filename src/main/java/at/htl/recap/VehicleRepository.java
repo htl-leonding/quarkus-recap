03bb8b171083576513ea2d5b197d7f9f91590a53
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-06-01 12:38:37 +0200

    VehicleRepositoryTest assertThat

--- src/main/java/at/htl/recap/VehicleRepository.java
+++ src/main/java/at/htl/recap/VehicleRepository.java
@@ -4,7 +4,9 @@ import at.htl.recap.entity.Vehicle;
 import io.quarkus.hibernate.orm.panache.PanacheRepository;
 
 import javax.enterprise.context.ApplicationScoped;
+import javax.transaction.Transactional;
 
 @ApplicationScoped
+@Transactional
 public class VehicleRepository implements PanacheRepository<Vehicle> {
 }
