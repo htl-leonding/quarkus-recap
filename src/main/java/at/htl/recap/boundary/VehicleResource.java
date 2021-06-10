be3d1b777a338f89cc26862ee524433a2052a06b
htl-leonding <t.stuetz@htl-leonding.ac.at>
2021-06-10 17:37:00 +0200

    Lecture 2021-06-08

--- src/main/java/at/htl/recap/boundary/VehicleResource.java
+++ src/main/java/at/htl/recap/boundary/VehicleResource.java
@@ -1,11 +1,59 @@
 package at.htl.recap.boundary;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
+import at.htl.recap.control.VehicleRepository;
+import at.htl.recap.entity.Vehicle;
+
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
 @Path("vehicle")
 public class VehicleResource {
 
+    @Inject
+    VehicleRepository vehicleRepository;
 
+
+    @Path("path/{name}")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String hello(@PathParam("name") String name) {
+        return String.format("hello %s" , name);
+    }
+
+    @Path("query")
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    public String helloQuery(
+            @QueryParam("grade") int grade,
+            @QueryParam("name") String name
+    ) {
+        return String.format("hello %d%s" ,grade, name);
+    }
+
+    @Path("object")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Vehicle getDummyVehicle() {
+        return new Vehicle("Opel", "Kadett", 1973);
+    }
+
+    @Path("response")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getDummyVehiclePerResponse() {
+        Vehicle opel = new Vehicle("Opel", "Kadett", 1973);
+        return Response.ok(opel).build();
+    }
+
+    @Path("db")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getVehicleFromDb() {
+        Vehicle opel = new Vehicle("Opel", "Kadett", 1973);
+        Vehicle kadett = vehicleRepository.save(opel);
+        return Response.ok(kadett).build();
+    }
 
 }
